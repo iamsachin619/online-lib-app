dfd2d2ba4a86398c977caa742a9f27e9939be7ad
rianarun <--rianarun@gmail.com>
2022-09-10 18:47:28 +0530

    arun

--- online-lib-backend/controllers/rent.js
+++ online-lib-backend/controllers/rent.js
@@ -1,16 +1,10 @@
 const mongoose = require("mongoose");
 const rentForm = require("../models/rentmodel");
 
-//renting the book
+
 function bookrentalcreatecltr(req, res){
     let dueDate = new Date()
-    todaysday = dueDate.getDate()
-    console.log(todaysday)
-    
-    // rentaldays= dueDate.setDate(todaysday + 10 ) //req.body.noOfDaysToRent
-    rentaldays = (todaysday + 30)
-    console.log(rentaldays)
-    console.log(dueDate)
+    //console.log(dueDate)
     dueDate.setDate(dueDate.getDate() + req.body.noOfDaysToRent)
 
  let rentData = rentForm.rentModel({
@@ -21,32 +15,30 @@ function bookrentalcreatecltr(req, res){
     amount: req.body.amount,
     approvedBy: "",
     lateFeeCharged: 0,
-    returnDate: "",
+    returnDate: new Date(),
     Status: "Pending"
     });
   
-//     rentData.save(err => { 
-//         if(err){
-//             console.log(err)
-//         }
-//         else{
-//             res.send({success: true });
-//         }})
+    rentData.save(err => { 
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send({success: true });
+        }})
     }
 
-    function bookrentalapprovalctrl(req,res){
+    // function bookrentalapprovalctrl(req,res){
 
-        let num = req.body._id
-        console.log(req.body._id) 
+    //     let rentData = rentForm.rentModel.findOneAndUpdate({Book_id:req.body.Book_id},{$set:{"status": "approve"}})
+    //     console.log(rentData)
 
-        let rentData = rentForm.rentModel.find({_id : req.body._id })
-        console.log(rentData)
-        //let rentData = rentForm.rentModel.findOneAndUpdate({Book_id: req.body.rental_id},{approvedBy:req.body.staff_id,status: 'approve'})
-        //let rentData = rentForm.rentModel.findOneAndUpdate({Book_id: tostring(req.body.Book_id)},{ $set: {approvedBy:'1234',status: 'approve'}})
+        // let rentData = rentForm.rentModel.findOne({Book_id: req.body.Book_id, User_id: req.body.User_id})
         // console.log(rentData)
-        res.send("let check")
+
+        // res.json({Book_id: rentData.Book_id, User_id: rentData.User_id }).status(200)
+            
         
-          
             // rentData.save(err => { 
             //     if(err){
             //         console.log(err)
@@ -56,6 +48,36 @@ function bookrentalcreatecltr(req, res){
             //     }})
             
         
-    }
+    // }
+
+
+    // function getRentallistctlr (req, res) {
+    //     rentForm.rentModel.find({
+    //         Book_id: req.body.Book_id,
+        // }, {
+        //     _id: 0,
+        //     __v: 0
+        // }, (err, data) => {
+        //     if (err) {
+        //         res.status(500).send({
+        //             success: false,
+        //             message: 'Something went wrong.'
+        //         });
+        //     } else if (data.length === 0) {
+        //         res.status(404).send({
+                   
+        //             success: false,
+        //             message: 'Invalid email provided.'
+        //         });
+        //     } else {
+        //         res.send(data);
+                
+        //     }
+        // });
+    // });
+
+
     
-module.exports = { bookrentalcreatecltr , bookrentalapprovalctrl}
+
+
+    module.exports = { bookrentalcreatecltr , bookrentalapprovalctrl}
--- online-lib-backend/index.js
+++ online-lib-backend/index.js
@@ -1,14 +1,10 @@
-var express = require('express')
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const session = require("express-session")
-const bookroutes = require('./routes/book')
+var express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const bookroutes = require('./routes/book');
 const userroutes =  require('./routes/user')
-<<<<<<< HEAD
-const rentroutes = require('./routes/rent')
-=======
 const rentRoute = require('./routes/rent')
->>>>>>> 25b6fed (rent process)
 const staffroutes =  require('./routes/user')
 const adminroutes =  require('./routes/admin')
 
@@ -19,13 +15,8 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname))
 app.use('/book', bookroutes)
 app.use('/user', userroutes)
-<<<<<<< HEAD
-app.use('/rent', rentroutes)
-app.use('/staff',staffroutes)
-=======
 app.use('/rent', rentRoute)
-app.user('/staff',staffroutes)
->>>>>>> 25b6fed (rent process)
+app.use('/staff',staffroutes)
 app.use('/admin', adminroutes)
 
 app.get("/",(req,res)=>{
