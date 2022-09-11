ca190582b257a6dd2b7888663dd16cbf72ee9892
rianarun <--rianarun@gmail.com>
2022-09-11 23:48:48 +0530

    Arun

--- online-lib-backend/controllers/rent.js
+++ online-lib-backend/controllers/rent.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const rentForm = require("../models/rentmodel");
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+const bookForm = require("../models/bookModel");
+
+>>>>>>> 2e9ed33 (Arun)
 
 //renting the book
 function bookrentalcreatecltr(req, res){
@@ -37,10 +42,27 @@ function bookrentalcreatecltr(req, res){
     }
 
     // function bookrentalapprovalctrl(req,res){
+<<<<<<< HEAD
 =======
 const bookForm = require("../models/bookModel");
 //renting the book
 async function bookrentalcreatecltr(req, res) {
+=======
+    async function bookrentalapprovalctrl(req,res){
+
+    //     let rentData = rentForm.rentModel.findOneAndUpdate({Book_id:req.body.Book_id},{$set:{"status": "approve"}})
+    //     console.log(rentData)
+
+        // let rentData = rentForm.rentModel.findOne({Book_id: req.body.Book_id, User_id: req.body.User_id})
+        // console.log(rentData)
+
+        // res.json({Book_id: rentData.Book_id, User_id: rentData.User_id }).status(200)
+            
+        let num = req.body._id
+        console.log(req.body._id) 
+
+        async function bookrentalcreatecltr(req, res) {
+>>>>>>> 2e9ed33 (Arun)
   let dueDate = new Date();
   todaysday = dueDate.getDate();
 
@@ -66,6 +88,7 @@ async function bookrentalcreatecltr(req, res) {
         // res.json({Book_id: rentData.Book_id, User_id: rentData.User_id }).status(200)
             
         
+<<<<<<< HEAD
             // rentData.save(err => { 
             //     if(err){
             //         console.log(err)
@@ -73,35 +96,49 @@ async function bookrentalcreatecltr(req, res) {
             //     else{
             //         res.send({success: true });
             //     }})
+=======
+        
+                           
+        
+    }
+  let rentData = rentForm.rentModel({
+    Book_id: req.body.book_id,
+    User_id: req.body.user_id,
+    createdDate: new Date(),
+    dueDate: new Date(),
+    approvalDate: new Date(),
+    amount: book.amountRate * req.body.noOfDaysToRent,
+    approvedBy: "",
+    lateFeeCharged: 0,
+    returnDate: "",
+    Status: "pending",
+    noOfDaysToRent:req.body.noOfDaysToRent
+
+  });
+
+      rentData.save(err => {
+          if(err){
+              console.log(err)
+          }
+          else{
+              res.send({success: true });
+          }})
+}
+
+
+        console.log(rentsByUser)
+        
+          let myBooks = await Promise.all(rentsByUser.map(async(rental)=>{
+            let book = await bookForm.bookModel.findOne({_id: rental.Book_id})  
+           return book
+>>>>>>> 2e9ed33 (Arun)
             
         
-    // }
+    
 
 
-    // function getRentallistctlr (req, res) {
-    //     rentForm.rentModel.find({
-    //         Book_id: req.body.Book_id,
-        // }, {
-        //     _id: 0,
-        //     __v: 0
-        // }, (err, data) => {
-        //     if (err) {
-        //         res.status(500).send({
-        //             success: false,
-        //             message: 'Something went wrong.'
-        //         });
-        //     } else if (data.length === 0) {
-        //         res.status(404).send({
-                   
-        //             success: false,
-        //             message: 'Invalid email provided.'
-        //         });
-        //     } else {
-        //         res.send(data);
-                
-        //     }
-        // });
-    // });
+    
+        
 
 
     
